89d11d326a92bd3256a553a83632c3114a71b6d6
Igor Amon <48227869+amonigor@users.noreply.github.com>
2021-05-30 14:16:56 -0300

    Adjust the tab and created home view

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,42 @@
 <template>
-    <div id="app">
-        <router-view/>
-    </div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
 export default {
-    name: "App",
+  name: "App",
 };
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Patua+One&family=Roboto&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Patua+One&family=Roboto&display=swap");
 
-html, body {
-    margin: 0;
-    padding: 0;
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
 }
 
 body {
-    display: flex;
+  display: flex;
 }
 
 #app {
-    font-family: 'Patua One', cursive;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #ffffff;
-    background-color: #000000;
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
+  font-family: "Patua One", cursive;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #ffffff;
+  background-color: #000000;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -15,6 +15,6 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
-    router,
-    render: h => h(App),
+  router,
+  render: h => h(App),
 }).$mount('#app')
