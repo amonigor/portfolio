c0ef94af1fe32b44cae152a226a8d4851ef32065
Igor Amon <48227869+amonigor@users.noreply.github.com>
2021-12-17 16:54:08 -0300

    added new images and SEO tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,29 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'author', name: 'author', content: 'Igor Amon' },
+      { hid: 'description', name: 'description', content: 'Igor Amon - Desenvolvedor Full Stack' },
+      { name: 'keys', content: 'fullstack developer, desenvolvedor fullstack, backend developer, desenvolvedor backend, frontend developer, desenvolvedor frontend, php, laravel, vuejs, vue, js, web, site, website' },
+      
+      // OpenGraph
+      { name: 'og:locale', content: 'pt_BR' },
+      { name: 'og:url', content: 'https://amonigor.dev/' },
+      { name: 'og:title', content: 'Igor Amon - Desenvolvedor Full Stack' },
+      { name: 'og:site_name', content: 'amonigor' },
+      { name: 'og:description', content: 'Olá! Me chamo Igor Amon e atuo como Desenvolvedor Full Stack, com experiência na área de desenvolvimento web.' },
+      { name: 'og:image', content: 'https://amonigor.dev/big-logo.png' },
+      { name: 'og:image:type', content: 'image/png' },
+      { name: 'og:image:width', content: '1000' },
+      { name: 'og:image:height', content: '1000' },
+      { name: 'og:type', content: 'website' },
+
+      // Twitter
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:url', content: 'https://amonigor.dev/' },
+      { name: 'twitter:title', content: 'Igor Amon - Desenvolvedor Full Stack' },
+      { name: 'twitter:description', content: 'Olá! Me chamo Igor Amon e atuo como Desenvolvedor Full Stack, com experiência na área de desenvolvimento web.' },
+      { name: 'twitter:image', content: 'https://amonigor.dev/big-logo.png' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
