1a3793503a401e18701982c3579925a5417766d4
Igor Amon <48227869+amonigor@users.noreply.github.com>
2021-12-21 20:26:33 -0300

    applied text shadow in blue text on about section

--- components/About.vue
+++ components/About.vue
@@ -53,11 +53,13 @@ export default {
           color: $blue-green-1;
           font-weight: 600;
           font-size: 1rem;
+          text-shadow: 0px 4px 5px rgba(0, 0, 0, 0.25);
         }
 
         a {
           color: $blue-green-1;
           font-size: 1rem;
+          text-shadow: 0px 4px 5px rgba(0, 0, 0, 0.25);
         }
       }
       padding-bottom: 60px;
