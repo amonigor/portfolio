82a5fabf4a43a0d97bcfcc9b285ae457a50b1b08
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-04-03 21:43:14 -0300

    update static information

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,15 +13,15 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'format-detection', content: 'telephone=no' },
       { hid: 'author', name: 'author', content: 'Igor Amon' },
-      { hid: 'description', name: 'description', content: 'Igor Amon - Desenvolvedor Full Stack' },
-      { name: 'keys', content: 'fullstack developer, desenvolvedor fullstack, backend developer, desenvolvedor backend, frontend developer, desenvolvedor frontend, php, laravel, vuejs, vue, js, web, site, website' },
+      { hid: 'description', name: 'description', content: 'Igor Amon - Software Engineer' },
+      { name: 'keys', content: 'software engineer, engenherio de software, frontend developer, desenvolvedor frontend, vuejs, vue, js, react, node, javascript, typescript, web, site, website' },
 
       // OpenGraph
       { name: 'og:locale', content: 'pt_BR' },
       { name: 'og:url', content: 'https://amonigor.dev/' },
-      { name: 'og:title', content: 'Igor Amon - Desenvolvedor Full Stack' },
+      { name: 'og:title', content: 'Igor Amon - Software Engineer' },
       { name: 'og:site_name', content: 'amonigor' },
-      { name: 'og:description', content: 'Olá! Me chamo Igor Amon e atuo como Desenvolvedor Full Stack, com experiência na área de desenvolvimento web.' },
+      { name: 'og:description', content: 'Olá! Me chamo Igor Amon e atuo como Engenherio de Software, com experiência na área de desenvolvimento web.' },
       { name: 'og:image', content: 'https://amonigor.dev/big-logo.png' },
       { name: 'og:image:type', content: 'image/png' },
       { name: 'og:image:width', content: '1000' },
@@ -32,7 +32,7 @@ export default {
       { name: 'twitter:card', content: 'summary' },
       { name: 'twitter:url', content: 'https://amonigor.dev/' },
       { name: 'twitter:title', content: 'Igor Amon - Desenvolvedor Full Stack' },
-      { name: 'twitter:description', content: 'Olá! Me chamo Igor Amon e atuo como Desenvolvedor Full Stack, com experiência na área de desenvolvimento web.' },
+      { name: 'twitter:description', content: 'Olá! Me chamo Igor Amon e atuo como Engenherio de Software, com experiência na área de desenvolvimento web.' },
       { name: 'twitter:image', content: 'https://amonigor.dev/big-logo.png' },
     ],
     link: [
