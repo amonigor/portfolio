97e1818bb5b90208eca47f5ce7e7da4af57a9bfa
Igor Amon <48227869+amonigor@users.noreply.github.com>
2024-06-11 10:02:20 -0300

    reset project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import "@/styles/globals.css";
+
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main>
+      <Head>
+        <title>Igor Amon</title>
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import { LinkIcon } from '@heroicons/react/16/solid';
-import Link from 'next/link';
+import Link from "next/link";
 
-const Home = () => {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-5">
       <h1 className="pb-5 text-9xl font-bold">Igor Amon</h1>
@@ -13,7 +12,6 @@ const Home = () => {
           className="flex gap-1 hover:opacity-50"
           target="_blank"
         >
-          <LinkIcon className="h-5 w-5" />
           <p className="leading-tight">LinkedIn</p>
         </Link>
         <Link
@@ -21,12 +19,9 @@ const Home = () => {
           className="flex gap-1 hover:opacity-50"
           target="_blank"
         >
-          <LinkIcon className="h-5 w-5" />
           <p className="leading-tight">Github</p>
         </Link>
       </div>
     </main>
   );
-};
-
-export default Home;
+}
