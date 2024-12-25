362a7ef28787772747a5555b860d28ccb6958226
Igor Amon <48227869+amonigor@users.noreply.github.com>
2024-12-25 14:47:10 -0300

    restart the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,6 @@
 import "@/styles/globals.css";
-
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
-import { cn } from "@/lib/utils";
-
-import ThemeProvider from "@/providers/theme-provider";
-import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <main
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable
-        )}
-      >
-        <Head>
-          <title>Igor Amon</title>
-        </Head>
-        <Component {...pageProps} />
-      </main>
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-5">
-      <h1 className="pb-5 text-9xl font-bold">Igor Amon</h1>
-      <p className="text-xl">Software Engineer</p>
-
-      <div className="flex items-center justify-center gap-2 pt-5">
-        <Link
-          href="https://www.linkedin.com/in/igor-amon/"
-          className="flex gap-1 hover:opacity-50"
-          target="_blank"
-        >
-          <p className="leading-tight">LinkedIn</p>
-        </Link>
-        <Link
-          href="https://github.com/amonigor"
-          className="flex gap-1 hover:opacity-50"
-          target="_blank"
-        >
-          <p className="leading-tight">Github</p>
-        </Link>
-      </div>
-    </main>
+    <div>
+      <h1>Home</h1>
+    </div>
   );
 }
