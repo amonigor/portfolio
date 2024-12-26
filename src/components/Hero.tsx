c67ff126708c1b854a23a5d4cf0c543d5bbb2cd7
Igor Amon <48227869+amonigor@users.noreply.github.com>
2024-12-26 15:30:16 -0300

    update padding in Hero component and enhance Mouse animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
 
   return (
     <section className="relative">
-      <p className="text-sm sm:text-lg md:text-xl text-center font-[poppins] pb-6 opacity-70">
+      <p className="text-sm sm:text-lg md:text-xl text-center font-[poppins] pb-2 sm:pb-4 md:pb-6 opacity-70">
         {t("welcome")}
       </p>
       <h1 className="text-5xl sm:text-7xl md:text-8xl text-center font-bold font-[poppins] pb-4 sm:pb-7 md:pb-8">
@@ -17,7 +17,7 @@ export default function Hero() {
       </p>
 
       <div className="flex flex-col items-center justify-center md:pt-32 pt-16">
-        <Mouse />
+        <Mouse className="motion-preset-oscillate motion-delay-100" />
         <ChevronDown className="motion-preset-oscillate" />
       </div>
     </section>
