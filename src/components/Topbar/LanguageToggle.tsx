7488e19e1975a66c539b558c9e649026f81d558c
Igor Amon <48227869+amonigor@users.noreply.github.com>
2024-12-25 22:17:10 -0300

    add language toggle

---
+++ src/components/Topbar/LanguageToggle.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Languages } from "lucide-react";
+import { useRouter } from "next/router";
+
+import Dropdown from "@/components/Topbar/Dropdown";
+
+export default function LanguageToggle() {
+  const router = useRouter();
+  const [locale, setLocale] = useState(
+    ["en-US", "pt-BR"].includes(router.locale as string)
+      ? router.locale
+      : "en-US"
+  );
+
+  const handleLocaleChange = (value: string) => {
+    router.push(router.asPath, router.asPath, { locale: value });
+    setLocale(value);
+  };
+
+  return (
+    <Dropdown
+      trigger={<Languages size={16} />}
+      value={locale}
+      items={[
+        { text: "English", value: "en-US" },
+        { text: "Português", value: "pt-BR" },
+      ]}
+      onSelect={(value) => handleLocaleChange(value)}
+    />
+  );
+}
--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,5 +1,7 @@
 import dynamic from "next/dynamic";
 
+import LanguageToggle from "@/components/Topbar/LanguageToggle";
+
 const DarkModeToggle = dynamic(
   () => import("@/components/Topbar/DarkModeToggle"),
   { ssr: false }
@@ -7,7 +9,8 @@ const DarkModeToggle = dynamic(
 
 export default function Topbar() {
   return (
-    <div className="absolute top-4 right-4">
+    <div className="absolute top-4 right-4 flex items-center gap-2">
+      <LanguageToggle />
       <DarkModeToggle />
     </div>
   );
