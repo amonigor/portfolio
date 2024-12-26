a497460203eac92e9bb09277b12d2c9e7ca71f80
Igor Amon <48227869+amonigor@users.noreply.github.com>
2024-12-26 15:43:06 -0300

    minor fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className="scroll-smooth">
       <Head />
       <body className="antialiased">
         <Main />
