a219657bdebb7e2856bb1095bf987af005383ff9
Igor Amon <48227869+amonigor@users.noreply.github.com>
2025-01-09 10:28:49 -0300

    hide unfinished items

--- src/components/Topbar/Navbar/Navbar.tsx
+++ src/components/Topbar/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { useTranslations } from "next-intl";
 
 import Desktop from "@/components/Topbar/Navbar/Desktop";
 import Mobile from "@/components/Topbar/Navbar/Mobile";
-import { MessageSquare, Puzzle, Terminal, User } from "lucide-react";
+import { Terminal, User } from "lucide-react";
 
 export default function Navbar() {
   const t = useTranslations("Navbar");
@@ -18,16 +18,16 @@ export default function Navbar() {
       href: "#skills",
       icon: <Terminal size={16} />,
     },
-    {
-      name: t("experience"),
-      href: "#experience",
-      icon: <Puzzle size={16} />,
-    },
-    {
-      name: t("contact"),
-      href: "#contact",
-      icon: <MessageSquare size={16} />,
-    },
+    // {
+    //   name: t("experience"),
+    //   href: "#experience",
+    //   icon: <Puzzle size={16} />,
+    // },
+    // {
+    //   name: t("contact"),
+    //   href: "#contact",
+    //   icon: <MessageSquare size={16} />,
+    // },
   ];
 
   return (
